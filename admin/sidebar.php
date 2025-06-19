dce7f3b71b47f19b4b85020be33c3a598f683473
Binayak Subba <subbabinayak21@gmail.com>
2025-06-19 11:04:48 +0530

    add logout out function and seperated sidebar from index

---
+++ admin/sidebar.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title></title>
+</head>
+<body>
+     <!-- Sidebar & Main Content Wrapper -->
+  <div class="d-flex" style="min-height: 100vh">
+    <!-- Sidebar -->
+    <div class="bg-primary text-white p-3" style="width: 250px;">
+      <h4 class="mb-4">Admin Panel</h4>
+      <ul class="nav flex-column">
+        <li class="nav-item"><a href="index.php" class="nav-link text-white">Dashboard</a></li>
+        <li class="nav-item"><a href="#" class="nav-link text-white">Doctors</a></li>
+        <li class="nav-item"><a href="#" class="nav-link text-white">Patients</a></li>
+        <li class="nav-item"><a href="#" class="nav-link text-white">Appointments</a></li>
+        <li class="nav-item"><a href="#" class="nav-link text-white">Reports</a></li>
+        <li class="nav-item"><a href="admin.php" class="nav-link text-white">Administrators</a></li>
+      </ul>
+    </div>
+
+</body>
+</html>
